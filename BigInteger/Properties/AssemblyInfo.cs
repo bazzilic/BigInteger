ede902f4b817c5f1b6afedce2e72156db74d0fa1
bazzilic <bazzilic@gmail.com>
2016-01-12 21:39:22 +0800

    Automated deployment to a NuGet feed (local) It was too hard to commit this into master, so alas. After merge of this branch it wouldn't matter anyway.

--- BigInteger/Properties/AssemblyInfo.cs
+++ BigInteger/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("BigInteger")]
 [assembly: AssemblyDescription("BigInteger class implementation for C#.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Vasily Sidorov (bazzilic) / Nanyang Technological University")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.04.*")]
+[assembly: AssemblyVersion("1.0.4.*")]
 //[assembly: AssemblyFileVersion("1.0.*")]
