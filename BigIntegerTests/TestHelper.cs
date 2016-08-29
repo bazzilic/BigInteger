e18ea2781864a1ac85c811c1fe4094b537cc9ab4
Divin Kong <kongdivin@gmail.com>
2016-08-30 00:03:41 +0800

    [Testing] Add and remove some comparison test cases.
    All individual comparison test case is replaced by one comparison test case which basically covers all comparison operators.

---
+++ BigIntegerTests/TestHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigIntegerTests
+{
+    public class TestHelper
+    {
+        private static int randSeed = 999;
+        public static int NUM_OF_TIMES_FOR_RANDOM = 999;
+
+        public static string GenRandNumStr(int numOfDigit)
+        {
+            if (numOfDigit <= 0) return "0";
+
+            var rand = new Random(randSeed);
+            randSeed = rand.Next();
+            string str = "";
+            if (rand.Next(100) > 50)
+                 str = "-";
+            str += rand.Next(1, 9).ToString();
+            for (int i = 1; i < numOfDigit - 1; i++)
+            {
+                str += rand.Next(9);
+            }
+            return str;
+        }
+
+        public static int CompareNumStr(string val1, string val2)
+        {
+            bool isVal1Pos = true, isVal2Pos = true;
+            if (val1[0] == '-')
+            {
+                isVal1Pos = false;
+                val1 = val1.Substring(1);
+            }
+
+            if (val2[0] == '-')
+            {
+                isVal2Pos = false;
+                val2 = val2.Substring(1);
+            }
+
+            if (isVal1Pos != isVal2Pos)
+                return isVal1Pos ? 1 : -1;
+
+
+            if (val1.Length != val2.Length)
+            {
+                return (val1.Length > val2.Length) == isVal1Pos ? 1 : -1;
+            }
+            else
+            {
+                for (int i = 0; i < val1.Length; i++)
+                {
+                    int c1 = Convert.ToInt32(val1[i]),
+                        c2 = Convert.ToInt32(val2[i]);
+                    if (c1 > c2)
+                        return isVal1Pos ? 1 : -1;
+                    else if (c1 < c2)
+                        return isVal1Pos ? -1 : 1;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ BigIntegerTests/TestHelperTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BigIntegerTests
+{
+    /// <summary>
+    /// Summary description for TestHelperTests
+    /// </summary>
+    [TestClass]
+    public class TestHelperTests
+    {
+        public TestHelperTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestCompareNumStr()
+        {
+            Assert.AreEqual(TestHelper.CompareNumStr("102", "102"), 0);
+            Assert.AreEqual(TestHelper.CompareNumStr("-102", "102"), -1);
+            Assert.AreEqual(TestHelper.CompareNumStr("108", "102"), 1);
+            Assert.AreEqual(TestHelper.CompareNumStr("1025", "102"), 1);
+            Assert.AreEqual(TestHelper.CompareNumStr("102", "1223"), -1);
+            Assert.AreEqual(TestHelper.CompareNumStr("-102", "-102"), 0);
+            Assert.AreEqual(TestHelper.CompareNumStr("1", "-102"), 1);
+
+            int num1 = 0, num2 = 0;
+            var rand = new Random();
+            for (int i = 0; i < TestHelper.NUM_OF_TIMES_FOR_RANDOM; i++)
+            {
+                num1 = rand.Next();
+                if (rand.Next(100) > 50)
+                    num1 = -num1;
+                num2 = rand.Next();
+                if (rand.Next(100) > 50)
+                    num2 = -num2;
+
+                switch(TestHelper.CompareNumStr(num1.ToString(), num2.ToString()))
+                {
+                    case 1:
+                        Assert.IsTrue(num1 > num2);
+                        break;
+                    case -1:
+                        Assert.IsTrue(num1 < num2);
+                        break;
+                    case 0:
+                        Assert.IsTrue(num1 == num2);
+                        break;
+                    default:
+                        Assert.IsFalse(true);
+                        break;
+                }
+
+            }
+        }
+
+        [TestMethod]
+        public void TestGenRandNumStr()
+        {
+            Random rand = new Random();
+            string str1;
+            Int64 num1 = 9223372036854775807;
+            for (int i = 0; i < TestHelper.NUM_OF_TIMES_FOR_RANDOM; i++)
+            {
+                str1 = TestHelper.GenRandNumStr(rand.Next(18));
+                Assert.IsTrue(Int64.TryParse(str1, out num1));
+                Assert.IsTrue(str1 == num1.ToString());
+            }
+        }
+    }
+}
